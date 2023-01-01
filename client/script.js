9343476d0c028f6ca669818d8bd4eff1bbf0274d
sushanthreddy <sushanthreddynalabolu@gmail.com>
2023-01-01 23:57:01 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -67,7 +67,7 @@ const handleSubmit=async(e)=>{
       'Content-Type':'application/json'
     },
     body:JSON.stringify({
-      prompt:data.get('prompt')
+      prompt:axios.data.get('prompt')
     })
   })
   clearInterval(loadInterval);
