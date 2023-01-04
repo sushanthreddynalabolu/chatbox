c7c8a8b5588a3ee614dda716ba0f34344dc57e02
sushanthreddy <sushanthreddynalabolu@gmail.com>
2023-01-04 17:30:42 +0530

    ok

--- client/script.js
+++ client/script.js
@@ -66,7 +66,8 @@ const handleSubmit=async(e)=>{
   const response= await fetch('https://chatbox-bqf2.onrender.com',{
     method:'POST',
     headers:{
-      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+      'Content-Type':'application/json',
+      // 'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
 
     
     },
